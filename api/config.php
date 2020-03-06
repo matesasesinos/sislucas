90453f5389847c95059acf4fabe55c43ad3d707c
matesasesinos <jeiriart@gmail.com>
2020-03-06 19:10:31 -0300

    nuevo diseño

--- api/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-$username = getenv('username');
-$password = getenv('password');
-$auth = getenv('auth');
